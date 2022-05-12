2936baf92883fa5cdcb237979931aff536d3e0f2
Diegoo11 <ynoacamino@gmail.com>
2022-05-11 23:27:27 -0500

    add ruter login and home

--- routers/index.js
+++ routers/index.js
@@ -3,14 +3,37 @@ const router = express.Router();
 const userExtractor = require('../userExtractor');
 const loginController = require('../controllers/loginController');
 const homeController = require('../controllers/homeController');
+const likeController = require('../controllers/likeController');
 
 router.get('/', homeController.homeGET);
+
+router.post('/', homeController.homePOST);
+
+router.get('/friend', userExtractor, homeController.friendGET);
+
+router.get('/users', userExtractor, homeController.usersGET);
+
+router.post('/addFriend', userExtractor, homeController.acceptFriendPOST); //input hiden
+
+router.get('/requestsFriend', userExtractor, homeController.requestFriendGET);
+
+router.post('/acceptFriend', userExtractor, homeController.acceptFriendPOST); //input hiden
+
+router.post('/rejectFriend', userExtractor, homeController.rejectFriendPOST); //input hiden
+
+router.post('/addComent', userExtractor, homeController.postComentPOST); //input hiden
+
+//////////////////////////////LIKE////////////////////////////////
+
+router.post('/like:id', userExtractor, likeController.like);
+
+router.post('/dislike:id', userExtractor, likeController.dislike);
 
 //////////////////////////////LOGIN////////////////////////////////
 
 router.get('/login', loginController.loginGET)
 
-router.get("/logout", (req, res) => {
+router.get("/logout", userExtractor, (req, res) => {
   req.logout();
   res.redirect("/");
 });
